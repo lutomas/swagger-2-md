9c12769d036ea82b90bfb7ecac121063b2971611
Tomas Lukosius <lutomas@gmail.com>
2020-12-03 15:23:07 +0200

    parse details.

--- types/swagger.go
+++ types/swagger.go
@@ -10,24 +10,52 @@ type Swagger struct {
 	Components *Components `json:"components"`
 }
 
-func (v *Swagger) WriteComponentsSchema(stdout *os.File) {
+func (v *Swagger) WriteComponentsSchema(stdout *os.File) error {
 	if v.Components != nil {
-		v.Components.WriteComponentsSchema(stdout)
+		return v.Components.WriteComponentsSchema(stdout)
 	}
+
+	return nil
 }
 
 type Components struct {
 	Schemas Schema `json:"schemas"`
 }
 
-func (v *Components) WriteComponentsSchema(stdout *os.File) {
+func (v *Components) WriteComponentsSchema(stdout *os.File) error {
 	if v.Schemas != nil {
-		writeComponentsSchema(stdout, v.Schemas)
+		return writeComponentsSchema(stdout, v.Schemas)
 	}
+	return nil
 }
 
 type Schema = map[string]interface{}
 
-func writeComponentsSchema(stdout *os.File, s Schema) {
-	fmt.Fprintln(stdout, "TODO WRITE")
+func writeComponentsSchema(stdout *os.File, s Schema) error {
+	required := map[string]bool{}
+	if s["required"] != nil {
+		data, err := asMap(s["required"])
+		if err != nil {
+			return err
+		}
+
+		required = parseRequired(data)
+	}
+
+	// required, properties (type, format, description),
+	fmt.Fprintln(stdout, "TODO WRITE", required)
+	return nil
+}
+
+func parseRequired(data map[string]interface{}) map[string]bool {
+	return map[string]bool{}
+}
+
+func asMap(i interface{}) (map[string]interface{}, error) {
+	switch v := i.(type) {
+	case map[string]interface{}:
+		return v, nil
+	}
+
+	return nil, fmt.Errorf("unsupported type: %T", i)
 }
